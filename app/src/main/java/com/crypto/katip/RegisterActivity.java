83d1ec8b9c5c8b2c2cc8b6bbd10679874038dc6e
Yasin <yasin.hizir@bil.omu.edu.tr>
2020-11-12 20:03:45 +0300

    RegisterActivity düzenlenmek için değiştirildi.

--- app/src/main/java/com/crypto/katip/RegisterActivity.java
+++ app/src/main/java/com/crypto/katip/RegisterActivity.java
@@ -13,47 +13,28 @@ import com.crypto.katip.controllers.SessionsController;
 import com.crypto.katip.models.User;
 
 public class RegisterActivity extends AppCompatActivity {
-    SessionsController session;
 
-    EditText usernameTextEdit;
-    EditText passwordTextEdit;
-    EditText passwordVerify;
+
+
     TextView notice;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
-
-        session = new SessionsController(getApplicationContext());
-
-        usernameTextEdit = findViewById(R.id.username);
-        passwordTextEdit = findViewById(R.id.password);
-        passwordVerify = findViewById(R.id.passwordVerify);
         notice = findViewById(R.id.notice);
     }
 
     @SuppressLint("SetTextI18n")
     public void register(View view) {
+        EditText usernameTextEdit = findViewById(R.id.username);
+        EditText passwordTextEdit = findViewById(R.id.password);
+        EditText passwordVerify = findViewById(R.id.passwordVerify);
+
         String username = usernameTextEdit.getText().toString();
         String password = passwordTextEdit.getText().toString();
         String passwordVerifyStr = passwordVerify.getText().toString();
 
-        User user = new User(username, password);
-
-        if ( username.equals("") || password.equals("") || passwordVerifyStr.equals("")) {
-            notice.setText("Lütfen istenilen alanları doğru bir şekilde doldurun");
-        }else if (!password.equals(passwordVerifyStr)){
-            notice.setText("Lütfen aynı şifreyi girdiğinizden emin olun");
-        }else {
-            if(user.save(getApplicationContext())) {
-                session.createLoginSession(user);
-                startActivity(new Intent(RegisterActivity.this, HomeActivity.class));
-                this.finish();
-            }else{
-                notice.setText("Kullanıcı adı daha önce kullanıldı!");
-            }
-        }
     }
 
     public void loginPage(View view) {
