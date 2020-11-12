a45c6563f650a0783861c9355e77384291105819
Yasin <yasin.hizir@bil.omu.edu.tr>
2020-11-12 21:51:38 +0300

    RegisterFormState'e passwordVerifyError eklendi.

--- app/src/main/java/com/crypto/katip/ui/register/RegisterFormState.java
+++ app/src/main/java/com/crypto/katip/ui/register/RegisterFormState.java
@@ -3,11 +3,13 @@ package com.crypto.katip.ui.register;
 public class RegisterFormState {
     private String usernameError;
     private String passwordError;
+    private String passwordVerifyError;
     private boolean isDataValid;
 
-    public RegisterFormState(String usernameError, String passwordError) {
+    public RegisterFormState(String usernameError, String passwordError, String passwordVerifyError) {
         this.usernameError = usernameError;
         this.passwordError = passwordError;
+        this.passwordVerifyError = passwordVerifyError;
     }
 
     public RegisterFormState(boolean isDataValid) {
@@ -20,6 +22,10 @@ public class RegisterFormState {
 
     public String getPasswordError() {
         return passwordError;
+    }
+
+    public String getPasswordVerifyError() {
+        return passwordVerifyError;
     }
 
     public boolean isDataValid() {
