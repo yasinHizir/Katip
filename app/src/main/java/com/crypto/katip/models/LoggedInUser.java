2ddd6974beca57a7fc6eb82ccb9c7e933b314046
Yasin <yasin.hizir@bil.omu.edu.tr>
2020-11-13 00:54:43 +0300

    LoggedInUser'a getter eklendi.

--- app/src/main/java/com/crypto/katip/models/LoggedInUser.java
+++ app/src/main/java/com/crypto/katip/models/LoggedInUser.java
@@ -8,4 +8,12 @@ public class LoggedInUser {
         this.id = id;
         this.username = username;
     }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
 }
