eb72ea77d35bd1fd5b5947f70c1ab9c5dcdcffc9
Yasin <yasin.hizir@bil.omu.edu.tr>
2020-11-12 20:09:14 +0300

    LoggedInUser oluşturuldu

---
+++ app/src/main/java/com/crypto/katip/models/LoggedInUser.java
@@ -0,0 +1,11 @@
+package com.crypto.katip.models;
+
+public class LoggedInUser {
+    private int id;
+    private String username;
+
+    public LoggedInUser(int id, String username) {
+        this.id = id;
+        this.username = username;
+    }
+}
