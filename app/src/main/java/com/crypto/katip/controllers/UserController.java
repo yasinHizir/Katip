7783b14861b7a06ca537d60ca940c6cf22d6a57d
Yasin <yasin.hizir@bil.omu.edu.tr>
2020-11-13 21:37:43 +0300

    UserController geliştirildi

--- app/src/main/java/com/crypto/katip/controllers/UserController.java
+++ app/src/main/java/com/crypto/katip/controllers/UserController.java
@@ -1,13 +1,48 @@
 package com.crypto.katip.controllers;
 
+import androidx.annotation.Nullable;
+
 import com.crypto.katip.models.User;
 
 public class UserController {
-    public User save(String username, String password) {
-        User user = new User();
-        user.setId(0);
-        user.setUsername(username);
-        user.setPassword(password);
-        return user;
+    public void save(String username, String password) {
+        User user = new User(username, password);
+        user.save();
+    }
+
+    public boolean isRegistered(String username, String password) {
+        User user = User.getInstance(username);
+        if (user != null) {
+            return user.isRegistered();
+        }
+        return false;
+    }
+
+    public void updatePassword(int id, String password) {
+        User user = User.getInstance(id);
+        if (user != null) {
+            user.setPassword(password);
+            user.update();
+        }
+    }
+
+    public void updateUsername(int id, String username) {
+        User user = User.getInstance(id);
+        if (user != null) {
+            user.setUsername(username);
+            user.update();
+        }
+    }
+
+    public void remove(int id) {
+        User user = User.getInstance(id);
+        if (user != null) {
+            user.remove();
+        }
+    }
+
+    @Nullable
+    public User getUser(String username) {
+        return User.getInstance(username);
     }
 }
