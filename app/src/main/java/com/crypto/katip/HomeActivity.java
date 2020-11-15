3793839e3e7162d534055397b8e41f3646e76f07
Yasin <yasin.hizir@bil.omu.edu.tr>
2020-11-15 17:22:44 +0300

    Çekme isteği öncesindeki son değişiklikler

--- app/src/main/java/com/crypto/katip/HomeActivity.java
+++ app/src/main/java/com/crypto/katip/HomeActivity.java
@@ -1,16 +1,14 @@
 package com.crypto.katip;
 
+import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-
-import androidx.appcompat.app.AppCompatActivity;
 
 public class HomeActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_home);
     }
-
 }
