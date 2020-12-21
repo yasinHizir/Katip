930ff23f6d4eb41c09c91e0350a4705afac9db5c
Yasin <yasin.hizir@bil.omu.edu.tr>
2020-12-21 17:31:49 +0300

    The content of the message to be sent has been changed

--- app/src/main/java/com/crypto/katip/communication/Envelope.java
+++ app/src/main/java/com/crypto/katip/communication/Envelope.java
@@ -3,16 +3,28 @@ package com.crypto.katip.communication;
 import java.io.Serializable;
 
 public class Envelope implements Serializable {
+    private final int type;
     private final String username;
+    private final int deviceId;
     private final byte[] body;
 
-    public Envelope(String username, byte[] body) {
+    public Envelope(int type, String username, int deviceId, byte[] body) {
+        this.type = type;
         this.username = username;
+        this.deviceId = deviceId;
         this.body = body;
+    }
+
+    public int getType() {
+        return type;
     }
 
     public String getUsername() {
         return username;
+    }
+
+    public int getDeviceId() {
+        return deviceId;
     }
 
     public byte[] getBody() {
