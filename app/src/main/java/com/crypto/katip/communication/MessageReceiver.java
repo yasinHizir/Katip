6b14a6046f072625b5bec9139dcfa5f4706e9d44
Yasin HIZIR <yasin.hizir@tutanota.com>
2021-06-06 20:29:07 +0300

    MessageReceiver edited because deserialize function is removed

--- app/src/main/java/com/crypto/katip/communication/MessageReceiver.java
+++ app/src/main/java/com/crypto/katip/communication/MessageReceiver.java
@@ -1,15 +1,11 @@
 package com.crypto.katip.communication;
 
-import androidx.annotation.Nullable;
-
 import com.rabbitmq.client.Channel;
 import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.ConnectionFactory;
 import com.rabbitmq.client.DeliverCallback;
 
-import java.io.ByteArrayInputStream;
 import java.io.IOException;
-import java.io.ObjectInputStream;
 import java.util.UUID;
 import java.util.concurrent.TimeoutException;
 
@@ -18,16 +14,14 @@ public class MessageReceiver {
     public void receive(UUID userUUID, ReceiveCallBack callBack){
         ConnectionFactory factory = new ConnectionFactory();
         String queueName = userUUID.toString();
-        factory.setHost("20.71.252.243");
+        factory.setHost("138.68.78.206");
 
         try {
             Connection connection = factory.newConnection();
             Channel channel = connection.createChannel();
 
             channel.queueDeclare(queueName, false, false, false, null);
-            DeliverCallback deliverCallback = (consumerTag, message) -> {
-                callBack.handleReceivedMessage( deserialize(message.getBody()));
-            };
+            DeliverCallback deliverCallback = (consumerTag, message) -> callBack.handleReceivedMessage( Envelope.deserialize(message.getBody()));
             while (true) {
                 channel.basicConsume(queueName, true, deliverCallback, consumerTag -> {});
                 if (Thread.interrupted()) {
@@ -44,17 +38,5 @@ public class MessageReceiver {
 
     public interface ReceiveCallBack {
         void handleReceivedMessage(Envelope envelope);
-    }
-
-    @Nullable
-    private Envelope deserialize(byte[] bytes) {
-        Envelope envelope = null;
-        try (ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(bytes)){
-            ObjectInputStream inputStream = new ObjectInputStream(byteArrayInputStream);
-            envelope = (Envelope) inputStream.readObject();
-        } catch (IOException | ClassNotFoundException e) {
-            e.printStackTrace();
-        }
-        return envelope;
     }
 }
